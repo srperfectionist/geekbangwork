7d279f145d9421c12cd1a7455d98683d9a16d225
sr <srperfectionist@outlook.com>
2022-03-27 09:00:48 +0800

    week11

---
+++ src/main/java/week11/pubsub/Publish.java
@@ -0,0 +1,31 @@
+package week11.pubsub;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.Random;
+
+/**
+ * @author shirui
+ * @date 2022/3/27
+ */
+public class Publish {
+
+    public Publish(JedisPool jedisPool, String channelName) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            int sleepTime = 0;
+            for (int i = 0; i < 10; i++) {
+                sleepTime = new Random().nextInt(10) + 1;
+
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                jedis.publish(channelName, "order sleep " + sleepTime);
+            }
+            jedis.publish(channelName, "");
+        }
+    }
+}
